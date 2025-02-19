96bfdd1cc245f6ce39ff7b8480d9edccf0570585
Manuel Florido <manuel.florido@sygel.es>
2025-02-19 11:37:52 +0100

    [ADD] Development status

--- base_old_migration_fields/__manifest__.py
+++ base_old_migration_fields/__manifest__.py
@@ -7,6 +7,7 @@
     "version": "15.0.1.0.0",
     "category": "Custom",
     "website": "https://github.com/sygel-technology/sy-server-backend",
+    "development_status": "Production/Stable",
     "author": "Sygel, Odoo Community Association (OCA)",
     "license": "AGPL-3",
     "application": False,
